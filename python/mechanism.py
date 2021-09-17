06ea72665913126edf7eafe811f9ed7fefc6f071
AlexKoldy <koldy@cooper.edu>
2021-09-17 18:45:21 -0400

    organized files

--- python/mechanism.py
+++ python/mechanism.py
@@ -1,71 +1,118 @@
-from differential import Differential
-from cs_gear import CS_Gear
+from matplotlib.pyplot import csd
 import numpy as np
 
-
 class Mechanism():
-    def __init__(self, beta):
-        '''Mechanical Constants'''
-        self.beta = beta
+	class Differential():
+		def __init__(self, mechanical_constants, q_0=np.zeros((2, 1))):
+			'''Monopole Gear state variables'''
+			self.q = q_0
+								
+			'''Mechanical constants'''
+			self.alpha = mechanical_constants[0]
+			self.beta = mechanical_constants[1]
+			self.gamma = mechanical_constants[2]
 
-        '''Establish Parts of the Mechanism'''
-        self.cs_gear = CS_Gear()
-        self.differential_A = Differential(self.cs_gear, np.pi/2, 0, 0, 'A')
-        self.differential_B = Differential(self.cs_gear, -np.pi/2, 0, np.pi, 'B')
+			'''Pose history'''
+			self.q_history = []
+			self.q_history.append(self.q)
 
-        '''Desired Final Euler Angles of CS Gear'''
-        self.phi_cs_des = 0 # rad; roll
-        self.theta_cs_des = np.pi/2 # rad; pitch
-        self.psi_cs_des = 0 # rad; yaw
-        self.q_cs_des = np.array([[self.phi_cs_des],
-                                 [self.theta_cs_des],
-                                 [self.psi_cs_des]])
+		def update(self, q):
+			self.q = q
+			self.q_history.append(self.q)
 
-        '''Generate Trajectory'''
-        self.q_cs_des_generated = self.generate_trajectory(self.q_cs_des)
+	class CrossSphericalGear():
+		def __init__(self, q_0=np.zeros((3, 1))):
+			'''Cross Spherial Gear state variables'''
+			self.q = q_0
+			
+			'''Pose history'''
+			self.q_history = []
+			self.q_history.append(self.q)
 
-        #self.cs_gear.update(self.cs_gear.q, self.q_cs_des_generated[:, 0].flatten())
+		def update(self, q):
+			self.q = q
+			self.q_history.append(self.q)
+		
+	def __init__(self):
+		'''Build differentials'''
+		self.differential_A = self.Differential(np.array([np.pi/2, 0, 0]))
+		self.differential_B = self.Differential(np.array([-np.pi/2, 0, np.pi]))
 
-        '''Inverse Kinematics'''
-        #self.differential_A.update()
-        #self.differential_B.update()
+		'''Build cross spherial gear'''
+		q_cs_0 = self.forward_kinematics(self.differential_A.q, self.differential_B.q)
+		self.cs_gear = self.CrossSphericalGear(q_cs_0)
 
-    def update(self, iterator):
-        '''Forward Kinematics
-        theta_A1 = self.differential_A.theta_1
-        theta_A2 = self.differential_A.theta_2
-        theta_B1 = self.differential_B.theta_1
-        theta_B2 = self.differential_B.theta_2
-        
-        U = (np.sin(self.beta)*np.cos(theta_A2)*np.sin(theta_A1)*np.sin(theta_A2) - np.cos(self.beta)*((np.sin(theta_A1)**2)*(np.sin(theta_A2)**2)+1))
-        V = ((np.sin(theta_A1)**2*np.sin(theta_A2)**2) - (np.sin(theta_A2)**2 + 1)) 
-        phi_cs = -np.arctan2((np.sin(theta_B1)*np.cos(theta_A1)*np.sin(theta_A1)*np.sin(theta_A2)**2 + np.cos(theta_B1)*U), ((np.cos(theta_B1)*np.cos(theta_A1)*np.sin(theta_A2)*(np.cos(self.beta)*np.sin(theta_A1)*np.sin(theta_A2) - np.sin(self.beta)*np.cos(theta_A2)) + np.sin(theta_B1)*V))) 
-        theta_cs = np.arcsin(np.sin(theta_A2)*(-np.cos(theta_A1)*np.cos(theta_A2)*np.sin(theta_B1) + np.cos(theta_A2)*np.cos(theta_B1)*np.sin(theta_A1)*np.cos(self.beta) + np.sin(theta_A2)*np.cos(theta_B1)*np.sin(self.beta))) 
-        psi_cs = -np.arctan2((np.sin(theta_A2) / np.cos(theta_A2)*(np.cos(theta_A1)*np.cos(theta_B1)*np.cos(self.beta) + np.sin(theta_A1)*np.sin(theta_B1))), 1) 
-        q_cs = np.array([[phi_cs],
-                         [theta_cs],
-                         [psi_cs]])
-        '''
-        
-        '''Update CS Gear'''
-        self.cs_gear.update(self.cs_gear.q, self.q_cs_des_generated[:, iterator])
-        
-        '''Inverse Kinematics'''
-        self.differential_A.update()
-        self.differential_B.update()
+		'''Generate trajectory of cross spherical gear'''
+		q_cs_des = np.array([[0], # desired roll [rad]
+							 [np.pi/2], # desired pitch [rad]
+							 [0]]) # desired yaw [rad]
+		self.q_cs_trajectory = self.generate_trajectory(self.cs_gear.q, q_cs_des)
 
-    def generate_trajectory(self, q_cs_des):
-        self.phi_cs_des_generated = np.linspace(self.cs_gear.q[0], q_cs_des[0], 1000).flatten()
-        self.theta_cs_des_generated = np.linspace(self.cs_gear.q[1], q_cs_des[1], 1000).flatten()
-        self.psi_cs_des_generated = np.linspace(self.cs_gear.q[2], q_cs_des[2], 1000).flatten()
-        
-        q_cs_des_generated = np.vstack((self.phi_cs_des_generated, self.theta_cs_des_generated, self.psi_cs_des_generated))
-        return q_cs_des_generated
+		print(self.cs_gear.q)
 
-        
-        
-        
-        
-        
+	def forward_kinematics(self, q_differential_A, q_differential_B):
+		beta = np.pi/2
 
-        
+		'''Convert true angles to joint angles'''
+		theta_A1 = q_differential_A.flatten()[0]
+		theta_A2 = q_differential_A.flatten()[1]/-2
+		theta_B1 = q_differential_B.flatten()[0]
+		theta_B2 = q_differential_B.flatten()[1]/-2
+		
+		'''Forward kinematics'''
+		U = (np.around(np.sin(beta), decimals=5)*np.cos(theta_A2)*np.sin(theta_A1)*np.sin(theta_A2) - np.around(np.cos(beta), decimals=5)*((np.sin(theta_A1)**2)*(np.sin(theta_A2)**2)+1))
+		V = ((np.sin(theta_A1)**2*np.sin(theta_A2)**2) - (np.sin(theta_A2)**2 + 1)) 
+		phi_cs = -np.arctan2((np.sin(theta_B1)*np.cos(theta_A1)*np.sin(theta_A1)*np.sin(theta_A2)**2 + np.cos(theta_B1)*U), ((np.cos(theta_B1)*np.cos(theta_A1)*np.sin(theta_A2)*(np.around(np.cos(beta), decimals=5)*np.sin(theta_A1)*np.sin(theta_A2) - np.around(np.around(np.sin(beta), decimals=5), decimals=5)*np.cos(theta_A2)) + np.sin(theta_B1)*V))) 
+		theta_cs = np.arcsin(np.sin(theta_A2)*(-np.cos(theta_A1)*np.cos(theta_A2)*np.sin(theta_B1) + np.cos(theta_A2)*np.cos(theta_B1)*np.sin(theta_A1)*np.around(np.cos(beta), decimals=5) + np.sin(theta_A2)*np.cos(theta_B1)*np.around(np.sin(beta), decimals=5))) 
+		psi_cs = -np.arctan2((np.sin(theta_A2) / np.cos(theta_A2)*(np.cos(theta_A1)*np.cos(theta_B1)*np.around(np.cos(beta), decimals=5) + np.sin(theta_A1)*np.sin(theta_B1))), 1) 
+		q_cs = np.array([[phi_cs], # roll [rad]
+                         [theta_cs], # pitch [rad]
+                         [psi_cs]]) # yaw [rad]
+
+		return q_cs
+	
+	def generate_trajectory(self, q_cs_0, q_cs_des):
+		phi_cs_trajectory = np.linspace(q_cs_0[0], q_cs_des[0], 1000).flatten()
+		theta_cs_trajectory = np.linspace(q_cs_0[1], q_cs_des[1], 1000).flatten()
+		psi_cs_trajectory = np.linspace(q_cs_0[2], q_cs_des[2], 1000).flatten()
+		q_cs_trajectory = np.vstack((phi_cs_trajectory, theta_cs_trajectory, psi_cs_trajectory))
+
+		return q_cs_trajectory
+
+	def inverse_kinematics(self, q_cs_des):
+		beta = np.pi/2
+
+		'''Inverse kinematics'''
+		theta_A1 = np.arctan2((np.cos(q_cs_des[0])*np.sin(q_cs_des[2]) + np.cos(q_cs_des[2])*np.sin(q_cs_des[1])*np.sin(q_cs_des[0])), (np.cos(q_cs_des[0])*np.cos(q_cs_des[2])*np.sin(q_cs_des[1]) - np.sin(q_cs_des[0])*np.sin(q_cs_des[2])))
+		theta_A2 = np.arccos(np.cos(q_cs_des[1])*np.cos(q_cs_des[2]))
+		theta_A3 = -np.arctan2(np.cos(q_cs_des[1])*np.sin(q_cs_des[2]), np.sin(q_cs_des[1]))
+		theta_B1 = -np.arctan2((np.cos(q_cs_des[2])*np.cos(beta)*np.cos(q_cs_des[0]) + np.sin(q_cs_des[2])*(np.sin(beta)*np.cos(q_cs_des[1]) - np.cos(beta)*np.sin(q_cs_des[1])*np.sin(q_cs_des[0]))), (-np.cos(beta)*np.sin(q_cs_des[1])*np.sin(q_cs_des[2]) + np.cos(q_cs_des[2])*np.sin(q_cs_des[2]))) 
+		theta_B2 = np.arccos(np.cos(q_cs_des[2])*np.cos(q_cs_des[0])*np.sin(beta) - np.sin(q_cs_des[2])*(np.cos(beta)*np.cos(q_cs_des[1]) + np.sin(beta)*np.sin(q_cs_des[1])*np.sin(q_cs_des[0]))) 
+		theta_B3 = np.arctan2((np.cos(q_cs_des[2])*(np.cos(beta)*np.cos(q_cs_des[1]) + np.sin(beta)*np.sin(q_cs_des[1])*np.sin(q_cs_des[0])) + np.sin(q_cs_des[2])*np.sin(beta)*np.cos(q_cs_des[0])), (-np.cos(beta)*np.sin(q_cs_des[1]) + (np.sin(beta)*np.cos(q_cs_des[1])*np.sin(q_cs_des[0])))) 
+
+		q_differential_A = np.array([[theta_A1], # roll [rad]
+									 [-2*theta_A2]]) # pitch [rad]
+		q_differential_B = np.array([[theta_B1], # roll [rad]
+									 [-2*theta_B2]]) # pitch [rad]
+		
+		return [q_differential_A, q_differential_B]
+	
+	def update(self, i):
+		'''
+		Inverse kinematics: map desired next cross spherical 
+		gear position to new differential positions
+		'''
+		q_differential_A, q_differential_B = self.inverse_kinematics(self.q_cs_trajectory[:, i])
+		self.differential_A.update(q_differential_A)
+		self.differential_B.update(q_differential_B)
+
+		'''
+		Forward kinematics: map new differential position to
+		new cross spherical gear position
+		'''
+		q_cs = self.forward_kinematics(self.differential_A.q, self.differential_B.q)
+		self.cs_gear.update(q_cs)
+
+
+
+		
